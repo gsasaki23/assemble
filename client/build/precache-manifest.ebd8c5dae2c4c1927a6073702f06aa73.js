365d5814153868e158ea2b33aefdfe63d596733f
Gaku Sasaki <reachgaku@gmail.com>
2020-05-06 23:53:33 -0700

    ran npm run build

---
+++ client/build/precache-manifest.ebd8c5dae2c4c1927a6073702f06aa73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a949d34015c15dd1225358f95fb80a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "271d03978bea4907f40f",
+    "url": "/static/css/2.b2ca442e.chunk.css"
+  },
+  {
+    "revision": "a9b6ccc45b600de611c5",
+    "url": "/static/css/main.49376b10.chunk.css"
+  },
+  {
+    "revision": "271d03978bea4907f40f",
+    "url": "/static/js/2.98ff1afa.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.98ff1afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b6ccc45b600de611c5",
+    "url": "/static/js/main.8832f6de.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
